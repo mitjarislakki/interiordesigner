536d95756d3fb12efdbd761b4c8268c1f2608525
rislakm1 <mitja.rislakki@aalto.fi>
2023-03-11 21:39:58 +0200

    Add fx template

--- src/test/scala/objectTest.scala
+++ src/test/scala/objectTest.scala
@@ -1,16 +1,45 @@
+import scalafx.application.JFXApp3
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+import scalafx.scene.effect.DropShadow
+import scalafx.scene.layout.HBox
+import scalafx.scene.paint.Color._
+import scalafx.scene.paint._
+import scalafx.scene.text.Text
 
-abstract class floorplanObject:
-  def lol: Unit
-
-trait shape extends floorplanObject:
-  def area: Double
-  def deez: Unit
-end shape
-
-object test extends shape:
-  def lol = println("lol")
-  def area = 2
-  def deez = println("deez")
-
-enum EditingMode:
-  case lol
+object ScalaFXHelloWorld extends JFXApp3 {
+  override def start(): Unit = {
+    stage = new JFXApp3.PrimaryStage {
+      //    initStyle(StageStyle.Unified)
+      title = "ScalaFX Hello World"
+      scene = new Scene {
+        fill = Color.rgb(38, 38, 38)
+        content = new HBox {
+          padding = Insets(50, 80, 50, 80)
+          children = Seq(
+            new Text {
+              text = "Scala"
+              style = "-fx-font: normal bold 100pt sans-serif"
+              fill = new LinearGradient(
+                endX = 0,
+                stops = Stops(Red, DarkRed))
+            },
+            new Text {
+              text = "FX"
+              style = "-fx-font: italic bold 100pt sans-serif"
+              fill = new LinearGradient(
+                endX = 0,
+                stops = Stops(White, DarkGray)
+              )
+              effect = new DropShadow {
+                color = DarkGray
+                radius = 15
+                spread = 0.25
+              }
+            }
+          )
+        }
+      }
+    }
+  }
+}
