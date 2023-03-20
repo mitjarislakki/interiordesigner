1b18dc60e6d75103e9d1d6468accda52a6346156
Mitja <mitja.rislakki@aalto.fi>
2023-03-20 16:53:08 +0200

    start implementing file reading

--- src/main/scala/ID/files/FileReader.scala
+++ src/main/scala/ID/files/FileReader.scala
@@ -3,5 +3,24 @@ package ID.files
 import java.io.*
 
 object Reader:
-  val fileIn = FileReader("placeholder.txt")
-  val lineReader = BufferedReader(fileIn)
+  def readFile(): Unit =
+    val fileIn =
+      try
+        FileReader("ExampleProject.YAML")
+      catch
+        case e:FileNotFoundException=> println("file not found"); return
+
+    val lineReader = BufferedReader(fileIn)
+
+    var projectName: Option[String] = None
+    var currentSection: Option[String] = None
+    var floorPlan: Option[String] = None
+    try
+      var inputLine = lineReader.readLine()
+      while inputLine != null do
+        inputLine = lineReader.readLine().trim
+        if inputLine.startsWith("Name:") && projectName.isEmpty then projectName = Some(inputLine.drop(5)) // add project name
+        if inputLine == "IDObjects:" then currentSection = Some(inputLine)
+
+    catch
+      case e: IOException => throw Error("Some weird IO error lol")
