e85b2c4ab977052bf3e225269a809936233bd428
Mitja <mitja.rislakki@aalto.fi>
2023-03-20 16:38:41 +0200

    further implement shape methods

--- src/main/scala/ID/projects/Shape.scala
+++ src/main/scala/ID/projects/Shape.scala
@@ -1,20 +1,28 @@
 package ID.projects
 import scalafx.scene.paint.Color
 
-trait Shape(private var _width: Double, private var _length: Double, private var _height: Double, private var _color: Color, val posOffset: (Int, Int, Int) = (0, 0, 0)):
+trait Shape(private var _width: Double, private var _length: Double, private var _height: Double, private var _color: Color, private var _pos: Pos, val posOffset: (Int, Int, Int) = (0, 0, 0)):
   def color = _color
   def width = _width
   def length = _length
   def height = _height
+  def pos = _pos
 
   def area: Double
-  def setColor(color: Color): Unit = _color = color
-  def setPos(pos: Pos): Unit
+
+  def setColor(color: Color): Unit =
+    _color = color
+  def setPos(pos: Pos): Unit =
+    _pos = pos
+
   def rotate(amount: Double): Unit
+
   def scaleX(amount: Double): Unit =
     _width = width * amount
+
   def scaleY(amount: Double): Unit =
     _length = length * amount
+
   def scale(amount: Double): Unit =
     scaleX(amount)
     scaleY(amount)
