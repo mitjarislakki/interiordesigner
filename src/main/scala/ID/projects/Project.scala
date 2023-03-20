38eb1502f5db4a623c2bf3551ae4341c8abe0cf4
Mitja <mitja.rislakki@aalto.fi>
2023-03-20 16:52:56 +0200

    change nothing

--- src/main/scala/ID/projects/Project.scala
+++ src/main/scala/ID/projects/Project.scala
@@ -1,10 +1,9 @@
 package ID.projects
 import scala.collection.mutable.Buffer
 
-object Project:
-  val objects = Buffer[IDObject]()
+class Project(val objects: Buffer[IDObject]):
 
   def objectsAtLayer(n: Int) = objects.filter(_.layer==n)
   def addObject(ob: IDObject): Unit = objects += ob
   def removeObject(ob: IDObject): Unit = ???
-  def transformObject(ob: IDObject, f: IDObject => IDObject): Unit = objects
+  def transformObject(ob: IDObject, f: IDObject => IDObject): Unit = ???
