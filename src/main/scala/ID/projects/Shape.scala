f52afd2473602960d0ced3c09d3fb81dc3465b89
Mitja <mitja.rislakki@aalto.fi>
2023-03-20 14:03:55 +0200

    Add classes Cuboid & Cylinder

--- src/main/scala/ID/projects/Shape.scala
+++ src/main/scala/ID/projects/Shape.scala
@@ -1,7 +1,7 @@
 package ID.projects
 import java.awt.Color
 
-trait Shape(length: Double, width: Double, height: Double, color: Color, posOffset: (Double, Double, Double)):
+trait Shape(width: Double, length: Double, height: Double, color: Color, posOffset: (Double, Double, Double) = (0, 0, 0)):
   def area: Double
   def setColor(color: Color): Unit
   def setPos(pos: Pos): Unit
@@ -9,3 +9,23 @@ trait Shape(length: Double, width: Double, height: Double, color: Color, posOffs
   def scaleX(amount: Double): Unit
   def scaleY(amount: Double): Unit
   def scale(amount: Double): Unit
+/*
+class Cuboid(width: Int, length: Int, height: Int, color: Color, posOffset: (Double, Double, Double)) extends Shape(length, width, height, color, posOffset):
+  def area = width * length
+  def setColor(color: Color) = ???
+  def setPos(amount: Double) = ???
+  def rotate(amount: Double) = ???
+  def scaleX(amount: Double) = ???
+  def scaleY(amount: Double) = ???
+  def scale(amount: Double) = ???
+
+
+class Cylinder(width: Int, length: Int, height: Int, color: Color, posOffset: (Double, Double, Double)) extends Shape(length, width, height, color, posOffset):
+  def area = Math.PI * width * length
+  def setColor(color: Color) = ???
+  def setPos(amount: Double) = ???
+  def rotate(amount: Double) = ???
+  def scaleX(amount: Double) = ???
+  def scaleY(amount: Double) = ???
+  def scale(amount: Double) = ???
+*/
