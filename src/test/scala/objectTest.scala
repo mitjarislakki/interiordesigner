1620978b0d676651d37bfb036e903802ac3501f9
rislakm1 <mitja.rislakki@aalto.fi>
2023-04-10 19:32:37 +0300

    added primitive scroll zoom that messes up dragging :D

--- src/test/scala/objectTest.scala
+++ src/test/scala/objectTest.scala
@@ -2,7 +2,7 @@ import ID.gui.IDToolbar.{background, orientation}
 import scalafx.application.JFXApp3
 import scalafx.geometry.{Insets, Point2D}
 import scalafx.scene.{Node, Scene}
-import scalafx.scene.input.{KeyEvent, MouseEvent}
+import scalafx.scene.input.{KeyEvent, MouseEvent, ScrollEvent}
 import scalafx.scene.layout.{Background, BackgroundFill, BorderPane, CornerRadii, GridPane, HBox, Pane, VBox}
 import scalafx.scene.paint.Color.*
 import scalafx.scene.paint.*
@@ -97,8 +97,11 @@ object graphicTest extends JFXApp3:
       wBox.setPromptText("0")
       val hBox = TextField()
       hBox.setPromptText("0")
-      val scaleBox = TextField()
-      scaleBox.setPromptText("0")
+      val scaleXBox = TextField()
+      scaleXBox.setPromptText("0")
+      val scaleYBox = TextField()
+      scaleYBox.setPromptText("0")
+
 
       properties.add(xLabel, 0, 0)
       properties.add(xBox, 1,  0)
@@ -108,30 +111,30 @@ object graphicTest extends JFXApp3:
       properties.add(wBox, 1, 2)
       properties.add(hLabel, 0, 3)
       properties.add(hBox, 1, 3)
-      properties.add(scaleBox, 1, 4)
-
+      properties.add(scaleXBox, 1, 4)
 
       selNode.onChange( (_, _, newVal) =>
         xBox.text = s"${newVal.getTranslateX}";
         yBox.text = s"${newVal.getTranslateY}";
-        hBox.text = "lmaooo"
+        scaleXBox.text = s"${newVal.getScaleX}";
+        scaleYBox.text = s"${newVal.getScaleY}"
         )
 
-      scaleBox.onKeyTyped = (event: KeyEvent) =>
-        selectedNode.foreach(node => node.scaleX = scaleBox.getText.toDoubleOption.getOrElse(node.getScaleX))
+      scaleXBox.onKeyTyped = (event: KeyEvent) =>
+        selectedNode.foreach(node => scaleXBox.getText.toDoubleOption.foreach(node.scaleX = _))
 
       xBox.onKeyTyped = (event: KeyEvent) =>
         selectedNode.foreach( node =>
           val c = xBox.getText.toDoubleOption match
-            case Some(p) if (p>0) => p
+            case Some(p) if (p>=0) => p
             case _ => node.getTranslateX
           ;
-          node.layoutX = c);
+          node.translateX = c);
 
       yBox.onKeyTyped = (event: KeyEvent) =>
         selectedNode.foreach( node =>
           val c = yBox.getText.toDoubleOption match
-            case Some(p) if (p>0) => p
+            case Some(p) if (p>=0) => p
             case _ => node.getTranslateY
           ;
           node.translateY = c);
@@ -139,6 +142,7 @@ object graphicTest extends JFXApp3:
       root.add(tb, 0, 0)
       root.add(editor, 1, 0)
       root.add(properties, 2, 0)
+      makeZoomable(editor)
 
 
       // TODO Drag selection
@@ -169,21 +173,27 @@ object graphicTest extends JFXApp3:
 */
   end start
 
+  def makeZoomable(node: Node) =
+    val dG = new DragContext
+    node.onScroll = (event: ScrollEvent) =>
+      val delta = event.getTextDeltaY
+      node.scaleX = node.getScaleX + delta/10
+      node.scaleY = node.getScaleY + delta/10
   def makeDraggable(node: Node): Unit =
-      val dG = new DragContext
-      node.onMousePressed = (event: MouseEvent) =>
-        if draggable then
-          dG.anchorX = event.screenX
-          dG.anchorY = event.screenY
-          dG.initX = node.getTranslateX
-          dG.initY = node.getTranslateY
-        event.consume()
+    val dG = new DragContext
+    node.onMousePressed = (event: MouseEvent) =>
+      if draggable then
+        dG.anchorX = event.screenX
+        dG.anchorY = event.screenY
+        dG.initX = node.getTranslateX
+        dG.initY = node.getTranslateY
+      event.consume()
 
-      node.onMouseDragged = (event: MouseEvent) =>
-        if draggable then
-          node.translateX = dG.initX + event.screenX - dG.anchorX
-          node.translateY = dG.initY + event.screenY - dG.anchorY
-        event.consume()
+    node.onMouseDragged = (event: MouseEvent) =>
+      if draggable then
+        node.translateX = scala.math.min(200  , scala.math.max(0, dG.initX + event.screenX - dG.anchorX))
+        node.translateY = scala.math.max(0, dG.initY + event.screenY - dG.anchorY)
+      event.consume()
   def makeScalable(node: Node): Unit =
     if scalable then ???
   def makeSelection = ???
