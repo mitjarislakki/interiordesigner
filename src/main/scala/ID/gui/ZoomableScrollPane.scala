3fe2388c8f3d24e604e21b2a5f8518e3a2a40c10
Mitja <mitja.rislakki@aalto.fi>
2023-05-11 21:17:02 +0300

    Fix scroll/zoom priority (event now consumed on ctrl+scroll zoom)

--- src/main/scala/ID/gui/ZoomableScrollPane.scala
+++ src/main/scala/ID/gui/ZoomableScrollPane.scala
@@ -3,35 +3,36 @@ import scalafx.scene.control.ScrollPane
 import scalafx.scene.input.ScrollEvent
 import scalafx.Includes.*
 import scalafx.event.EventIncludes.eventClosureWrapperWithParam
-import scalafx.geometry.Insets
+import scalafx.geometry.{Insets, Pos}
 import scalafx.scene.{Group, Node}
-import scalafx.scene.layout.CornerRadii
+import scalafx.scene.layout.{CornerRadii, StackPane, VBox}
 
 class ZoomableScrollPane(child: Node) extends ScrollPane:
   val bgfill = scalafx.scene.layout.BackgroundFill(scalafx.scene.paint.Color.LightSkyBlue, CornerRadii.Empty, Insets.Empty)
   val bg = new scalafx.scene.layout.Background(Array(bgfill))
   this.setBackground(bg)
-  val inner = Group(child)
-  content = inner
+  val outer = Group(child)
+  content = outer
   pannable = false
-  fitToHeight = false
-  fitToWidth = false
+  fitToHeight = true
+  fitToWidth = true
 
-  this.addEventHandler(ScrollEvent.Scroll, (event) =>
-    val zoomFactor = 1.1
-    val deltaY = event.getDeltaY
-    val deltaX = event.getDeltaX
+  outer.addEventHandler(ScrollEvent.Scroll, (event) =>
     if event.isControlDown then
+      event.consume()
+      val zoomFactor = 1.1
+      val deltaY = event.getDeltaY
+      val deltaX = event.getDeltaX
       if deltaY < 0 then
         child.scaleX.value /= zoomFactor ;
         child.scaleY.value /= zoomFactor
       else if deltaY > 0 then
         child.scaleX.value *= zoomFactor ;
         child.scaleY.value *= zoomFactor ;
-      event.consume();
-    else
-      child.translateX = child.getTranslateX + deltaX;
-      child.translateY = child.getTranslateY + deltaY
+      ;
+      else
+        child.translateX = child.getTranslateX + deltaX;
+        child.translateY = child.getTranslateY + deltaY
 
   )
 end ZoomableScrollPane
