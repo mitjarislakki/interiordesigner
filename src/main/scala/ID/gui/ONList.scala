1be592fa780beca8d60c2665e8d5a45cc117c4c4
Mitja <mitja.rislakki@aalto.fi>
2023-05-14 17:58:55 +0300

    Implement drag&drop list, still missing bind to ObjectNodes in editor

--- src/main/scala/ID/gui/ONList.scala
+++ src/main/scala/ID/gui/ONList.scala
@@ -1,18 +1,81 @@
 package ID.gui
 
 import scalafx.collections.ObservableBuffer
-import scalafx.scene.control.{ListCell, ListView}
+import scalafx.scene.control.{ContentDisplay, ListCell, ListView}
 import scalafx.scene.layout.{GridPane, Region}
 import javafx.scene.Node
 import scalafx.geometry.Pos
+import scalafx.scene.input.{ClipboardContent, DragEvent, MouseDragEvent, MouseEvent, TransferMode}
+import scalafx.Includes.*
+
+import java.util
 
 
 object ONList extends GridPane():
   minWidth = Region.USE_PREF_SIZE
   def setInput(input: ObservableBuffer[Node]) = println("This is a temporary setInput method for ONList.scala // TODO")
-  val contents = List("Bananas", "Apples", "Pairs", "Lychees", "Strawberries", "Blueberries")
-  val nodeList = new ListView(contents)
+  val contents: ObservableBuffer[String] = ObservableBuffer("Bananas", "Apples", "Pairs", "Lychees", "Strawberries", "Blueberries")
+  private val contBuf = ObservableBuffer[String]()
+  val nodeList = new ListView(contents):
+    cellFactory = ((param: ListView[String]) => new oNCell())
   add(nodeList, 0, 0)
   def setContent(nodes: Iterable[ObjectNode]) = ???
-  class ObjectNodeCell extends ListCell[String]:
+
+
+  private class oNCell() extends scalafx.scene.control.ListCell[String]:
+    val thisCell: ListCell[_] = this
+    contentDisplay = ContentDisplay.TextOnly
     alignment = Pos.Center
+
+
+    this.onDragDetected = (event: MouseEvent) =>
+      if !(this.getItem == null) then
+        val items = this.getListView.getItems
+        val dragboard = startDragAndDrop(TransferMode.Move)
+        val content = new ClipboardContent
+        content.putString(this.getItem)
+        dragboard.setContent(content)
+        event.consume()
+
+
+    this.onDragOver = (event: DragEvent) =>
+      if (event.gestureSource.ne(thisCell) && event.getDragboard.hasString) then
+        event.acceptTransferModes(TransferMode.Move)
+      event.consume()
+
+
+    this.onDragEntered = (event: DragEvent) =>
+      if (event.getGestureSource.ne(thisCell) && event.getDragboard.hasString) then
+        this.setOpacity(0.3)
+
+
+    this.onDragExited = (event: DragEvent) =>
+      if (event.getGestureSource.ne(thisCell) && event.getDragboard.hasString) then
+        this.setOpacity(1)
+
+
+    this.onDragDropped = (event: DragEvent) =>
+      if !(this.getItem == null) then
+        val db = event.getDragboard
+        var success = false
+        if db.hasString then
+          val items = this.getListView.getItems
+          val draggedIdx = items.indexOf(db.getString)
+          val thisIdx = items.indexOf(this.getItem)
+          items.set(draggedIdx, this.getItem)
+          items.set(thisIdx, db.getString)
+          val itemsCopy = new util.ArrayList[String](items)
+          this.getListView.getItems.setAll(itemsCopy)
+          success = true
+        event.setDropCompleted(success)
+        event.consume()
+
+
+    this.onDragDone = e  => e.consume()
+
+
+    def updateItem(item: String, empty: Boolean): Unit =
+        this.text = item
+
+
+    this.item.onChange( (_, _, newVal) => text = newVal)
