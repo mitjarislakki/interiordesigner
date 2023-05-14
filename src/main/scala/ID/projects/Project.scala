ecd94783ffcff05bc2f59744c20895cbddb12ede
Mitja <mitja.rislakki@aalto.fi>
2023-05-15 02:20:08 +0300

    Remove unnecessary val

--- src/main/scala/ID/projects/Project.scala
+++ src/main/scala/ID/projects/Project.scala
@@ -6,6 +6,6 @@ import ID.files.ONContender
 import scalafx.scene.shape.Shape
 import ID.gui.ObjectNode
 
-class Project(val name: String, val dimensions: (Double, Double, Double), val furniture: Seq[(String, Seq[(Shape, Pos)], Int, Double, Double)] = Seq()):
+class Project(val name: String, dimensions: (Double, Double, Double), val furniture: Seq[(String, Seq[(Shape, Pos)], Int, Double, Double)] = Seq()):
   val (initLength, initWidth, initHeight) = dimensions
   def initializeNodes = furniture.map(ObjectNode(_))
